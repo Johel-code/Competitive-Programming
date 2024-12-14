eace4fc83fb10f3a1a6fa40b693dbd2ccb7f9232
Johel-code <jbsch16@gmail.com>
2024-12-14 19:26:31 -0400

    refactor code en linux

--- cursoJava/ProblemMoneda/Robot.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.ArrayList;
-
-public class Robot {
-    
-    private ArrayList<Moneda> monedas;
-    private Balanza balanza;
-}
--- tallerUmss/Frog1.java
+++ tallerUmss/Frog1.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 
 public class Frog1 {
-    
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int [] dp = new int[n];
+  public static void main(String[] args) {
 
-        arr[0] = sc.nextInt();
-        arr[1] = sc.nextInt();
+    Scanner sc = new Scanner(System.in);
 
-        dp[0] = 0;
-        dp[1] = Math.abs(arr[1] - arr[0]);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    int[] dp = new int[n];
 
-        for (int i = 2; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        
-        for (int i = 2; i < n; i++) {
-            int mov1 = Math.abs(arr[i]-arr[i-1]) + dp[i-1];
-            int mov2 = Math.abs(arr[i]-arr[i-2]) + dp[i-2];
-            dp[i] = Math.min(mov1, mov2);
-        }
+    arr[0] = sc.nextInt();
+    arr[1] = sc.nextInt();
 
-        System.out.println(dp[n-1]);
+    dp[0] = 0;
+    dp[1] = Math.abs(arr[1] - arr[0]);
 
-        sc.close();
+    for (int i = 2; i < n; i++) {
+      arr[i] = sc.nextInt();
     }
+
+    for (int i = 2; i < n; i++) {
+      int mov1 = Math.abs(arr[i] - arr[i - 1]) + dp[i - 1];
+      int mov2 = Math.abs(arr[i] - arr[i - 2]) + dp[i - 2];
+      dp[i] = Math.min(mov1, mov2);
+    }
+
+    System.out.println(dp[n - 1]);
+
+    sc.close();
+  }
 }
